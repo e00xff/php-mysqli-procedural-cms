77fc963e60574c8593330ee8201f0c94e1e09b87
Levan <e00xff@gmail.com>
2020-02-01 15:11:26 -0800

    Inserting data into category table and displaying it

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,29 +1,34 @@
+<div class="card mb-3 shadow-sm">
+    <div class="card-body">
+        <ul class="list-unstyled mb-0">
+            <?php
+            $query = "SELECT * FROM categories";
+            $result = mysqli_query($connection, $query);
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $title = $row['title'];
+                $slug = $row['slug'];
+                ?>
+                <li>
+                    <a href="#"><?php echo $title; ?></a>
+                </li>
+                <?php
+            }
+            ?>
+        </ul>
+    </div>
+</div>
+
 <form method="get" action="search.html" class="mb-3">
     <div class="input-group">
         <input class="form-control py-2" type="search" id="search-input" placeholder="Search article">
         <span class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="submit">
-                                <i class="fa fa-search"></i>
-                            </button>
-                        </span>
+            <button class="btn btn-outline-secondary" type="submit">
+                <i class="fa fa-search"></i>
+            </button>
+        </span>
     </div>
 </form>
-
-<div class="card mb-3 shadow-sm">
-    <div class="card-body">
-        <ul class="list-unstyled mb-0">
-            <li><a href="category.html">News</a></li>
-            <li><a href="#">Health</a></li>
-            <li><a href="#">Planet Earth</a></li>
-            <li><a href="#">Strange News</a></li>
-            <li><a href="#">Space & Physics</a></li>
-            <li><a href="#">Animals</a></li>
-            <li><a href="#">History</a></li>
-            <li><a href="#">Tech</a></li>
-            <li><a href="#">Culture</a></li>
-        </ul>
-    </div>
-</div>
 
 <div class="card mb-3 shadow-sm">
     <div class="card-header">Recent Posts</div>
