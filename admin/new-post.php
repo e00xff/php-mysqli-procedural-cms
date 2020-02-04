410294eb13ccdd4ba52b5b781f85bf63bdf27ab5
Levan <e00xff@gmail.com>
2020-02-04 09:14:30 -0800

    Creating post in admin panel;

--- admin/new-post.php
+++ admin/new-post.php
@@ -25,7 +25,7 @@
                         <div class="card-header">
                             <h3 class="card-title">New Post</h3>
                         </div>
-                        <form method="post" action="#" role="form">
+                        <form action="posts.php?source=new-post" method="post" role="form">
                             <div class="card-body">
                                 <div class="form-group">
                                     <label for="title">Title</label>
@@ -33,7 +33,7 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="status">Status</label>
-                                    <select name="status" id="status" class="form-control">
+                                    <select id="status" name="status" class="form-control">
                                         <option selected disabled>- Select Status -</option>
                                         <option value="published">Published</option>
                                         <option value="unpublished">Unpublished</option>
@@ -42,6 +42,10 @@
                                 <div class="form-group">
                                     <label for="tags">Tags</label>
                                     <input type="text" class="form-control" id="tags" name="tags">
+                                </div>
+                                <div class="form-group">
+                                    <label for="author">Author</label>
+                                    <input type="text" class="form-control" id="author" name="author">
                                 </div>
                                 <div class="form-group">
                                     <label for="category">Category</label>
@@ -60,23 +64,47 @@
                                 <div class="form-group">
                                     <label for="photo">Photo</label>
                                     <div class="custom-file">
-                                        <input type="file" class="custom-file-input" id="photo" name="photo" required>
+                                        <input type="file" class="custom-file-input" id="photo" name="photo">
                                         <label class="custom-file-label" for="photo">Choose file...</label>
                                     </div>
                                 </div>
                                 <div class="form-group mb-0">
-                                    <label for="description">Description</label>
-                                    <textarea class="form-control textarea" id="description" name="description"></textarea>
+                                    <label for="excerpt">Excerpt</label>
+                                    <textarea class="form-control textarea" id="excerpt" name="excerpt" rows="3"></textarea>
                                 </div>
-
+                                <div class="form-group mb-0">
+                                    <label for="content">Description</label>
+                                    <textarea class="form-control textarea" id="content" name="content" rows="5"></textarea>
+                                </div>
                             </div>
-
                             <div class="card-footer">
-                                <button type="submit" class="btn btn-primary btn-sm btn-flat">Add</button>
+                                <button type="submit" name="submit" class="btn btn-primary btn-sm btn-flat">Add</button>
                                 <button type="reset" class="btn btn-info btn-sm btn-flat">Cancel</button>
                             </div>
                         </form>
                     </div>
+                    <?php
+                    if (isset($_POST['submit'])) {
+                        $postCategory = $_POST['category'];
+                        $postTitle = $_POST['title'];
+                        $postStatus = $_POST['status'];
+                        $postTags = $_POST['tags'];
+                        $postCommentCount = NULL;
+                        $postAuthor = $_POST['author'];
+                        $postDate = date("Y/m/d");
+                        $postPhoto = NULL;
+                        $postExcerpt = $_POST['excerpt'];
+                        $postContent = $_POST['content'];
+
+                        $query = "INSERT INTO posts(`category_id`, `title`, `status`, `tags`, `comment_count`, `author`, `date`, `photo`, `excerpt`, `content`) ";
+                        $query .= "VALUES('$postCategory', '$postTitle', '$postStatus', '$postTags', '$postCommentCount', '$postAuthor', '$postDate', '$postPhoto', '$postExcerpt', '$postContent')";
+                        $result = mysqli_query($connection, $query) or die('Query Error: '.mysqli_error($connection));
+
+                        if ($result) {
+                            echo '<p>Post inserted, <a href="posts.php?source=view-posts">view post</a></p>';
+                        }
+                    }
+                    ?>
                 </div>
             </div>
         </div>
