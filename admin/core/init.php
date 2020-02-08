034eeb5cc9a21b2ca35c76c432dc2229c4acd11a
Levan <e00xff@gmail.com>
2020-02-08 12:00:15 -0800

    Improved logout page validation

--- admin/core/init.php
+++ admin/core/init.php
@@ -2,15 +2,15 @@
 ob_start();
 session_start();
 
-//if (!isset($_SESSION['role'])) {
-//    header("Location: ../index.php");
-//}
-
-if (isset($_SESSION['role'])) {
-    if ($_SESSION['role'] !== 'administrator') {
-        header('Location: ../login.php');
-    }
+if (!isset($_SESSION['role'])) {
+    header("Location: ../index.php");
 }
+
+//if (isset($_SESSION['role'])) {
+//    if ($_SESSION['role'] !== 'administrator') {
+//        header('Location: ../login.php');
+//    }
+//}
 
 require 'database.php';
 require 'functions.php';
