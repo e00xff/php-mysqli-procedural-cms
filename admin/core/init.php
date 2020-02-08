4bc4182120f23cc080230ca8c0ffc665c9ea067e
Levan <e00xff@gmail.com>
2020-02-08 11:47:58 -0800

    Validating user admin

--- admin/core/init.php
+++ admin/core/init.php
@@ -1,6 +1,17 @@
 <?php
 ob_start();
 session_start();
+
+//if (!isset($_SESSION['role'])) {
+//    header("Location: ../index.php");
+//}
+
+if (isset($_SESSION['role'])) {
+    if ($_SESSION['role'] !== 'administrator') {
+        header('Location: ../login.php');
+    }
+}
+
 require 'database.php';
 require 'functions.php';
 require 'helpers.php';
