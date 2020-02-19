57f1cd6240e784c820e4a1c89689b383dc9dd33a
Levan <e00xff@gmail.com>
2020-02-19 19:36:40 +0400

    modal delete confirm

--- admin/includes/scripts.php
+++ admin/includes/scripts.php
@@ -3,14 +3,18 @@
 <script src="dist/js/adminlte.min.js"></script>
 
 <script>
-    // Loader
+    /*********************************************
+    * Loader
+    **********************************************/
     var divBox = '<div class="load-screen"><div class="loading"></div></div>';
     $("body").prepend(divBox);
     $(".load-screen").delay(300).fadeOut(200, function () {
         $(this).remove();
     });
 
-    // Online users using ajax
+    /*********************************************
+     * Online users using ajax
+     **********************************************/
     function loadUsersOnline() {
         $.get("core/functions.php?onlineUsers=result", function (data) {
             $(".users-online").text(data);
@@ -20,4 +24,21 @@
     setInterval(function () {
         loadUsersOnline();
     }, 500);
+
+
+    /*********************************************
+     * Delete record
+     **********************************************/
+
+    $(document).ready(function () {
+        $('.deleteRecord').on('click', function () {
+            var id = $(this).attr("rel");
+            var deleteURL = "posts.php?delete="+id+" ";
+
+            $('.modal-delete-record').attr("href", deleteURL);
+            $('#modal-delete').modal('show');
+
+        });
+    });
+
 </script>
--- admin/view-posts.php
+++ admin/view-posts.php
@@ -184,7 +184,8 @@
                                                     <a href="posts.php?source=edit-post&postID=<?php echo $postID; ?>" class="btn btn-primary btn-xs btn-flat" title="Edit Post"><i class="far fa-edit"></i></a>
                                                     <a href="posts.php?published=<?php echo $postID; ?>" class="btn btn-success btn-xs btn-flat <?php echo $postStatus == 'published' ? 'disabled' : ''; ?>" title="Publish Post"><i class="fas fa-check"></i></a>
                                                     <a href="posts.php?unpublished=<?php echo $postID; ?>" class="btn btn-warning btn-xs btn-flat <?php echo $postStatus == 'unpublished' ? 'disabled' : ''; ?>" title="Unpublish Post"><i class="fas fa-ban"></i></a>
-                                                    <a href="posts.php?delete=<?php echo $postID; ?>" class="btn btn-danger btn-xs btn-flat" onclick="return confirm('Are you sure you want to delete this record?');" title="Remove Post"><i class="far fa-trash-alt"></i></a>
+                                                    <a href="javascript:void(0)" rel="<?php echo $postID; ?>" class="btn btn-danger btn-xs btn-flat deleteRecord" data-toggle="modal" data-target="#modal-delete" title="Remove Post"><i class="far fa-trash-alt"></i></a>
+                                                    <!--<a href="posts.php?delete=<?php echo $postID; ?>" class="btn btn-danger btn-xs btn-flat" onclick="return confirm('Are you sure you want to delete this record?');" title="Remove Post"><i class="far fa-trash-alt"></i></a>-->
                                                     <!-- <button onclick="deleteRecord()" id="deleteRecord" type="button" class="btn btn-danger btn-xs btn-flat"><i class="far fa-trash-alt"></i></button> -->
                                                 </td>
                                             </tr>
@@ -255,6 +256,26 @@
                     }
 
                     ?>
+
+                    <div class="modal fade" id="modal-delete">
+                        <div class="modal-dialog modal-sm">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Delete Record</h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    Are you sure you want to delete this post ?
+                                </div>
+                                <div class="modal-footer justify-content-between">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                                    <a href="#" class="btn btn-danger modal-delete-record">Yes</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                     <a href="posts.php?source=new-post" class="btn btn-success btn-sm btn-flat mb-3">New Post</a>
 
